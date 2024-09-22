06c4529dcf9ebaf99123749c5f9540f8548390ae
Mohamed Benabdellah <77970397+Pooreffects@users.noreply.github.com>
2024-09-22 21:54:10 +0100

    feat: Custom Button that switches to the Customizer

--- client/src/components/CustomButton.tsx
+++ client/src/components/CustomButton.tsx
@@ -1,5 +1,45 @@
-export default function CustomButton() {
+interface IButtonProps {
+  title: string;
+  type?: 'filled' | 'outlined';
+  customStyles?: string;
+  handleClick: () => void;
+}
+import { useSnapshot } from 'valtio';
+import state from '../store';
+
+export default function CustomButton({
+  title,
+  type,
+  customStyles,
+  handleClick,
+}: IButtonProps) {
+  const snap = useSnapshot(state);
+
+  const generateStyle = () => {
+    switch (type) {
+      case 'filled':
+        return {
+          backgroundColor: snap.color,
+          color: '#fff',
+        };
+      case 'outlined':
+        return {
+          backgroundColor: 'transparent',
+          color: '#000',
+          border: '1px solid #000',
+        };
+      default:
+        return {};
+    }
+  };
+
   return (
-    <button type="button"></button>
-  )
+    <button
+      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+      style={generateStyle()}
+      onClick={handleClick}
+    >
+      {title}
+    </button>
+  );
 }
